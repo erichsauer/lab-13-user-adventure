7ee35ce6b8c02e01be00424cea2910523563f3ec
ERICH SAUER <erich.sauer@gmail.com>
2021-01-28 14:37:04 -0800

    completes map, quest pages functionality

--- map/map.js
+++ map/map.js
@@ -1,18 +1,35 @@
 // import functions and grab DOM elements
 import questData from '../data.js';
+import { getFromLocalStorage } from '../localStorageUtils.js';
+import { QUESTER } from '../magicStrings.js';
 
 const ul = document.querySelector('ul');
+const user = getFromLocalStorage(QUESTER);
 // initialize state
+let completedAllQuests = true;
 
 // set event listeners to update state and DOM
 for (const quest of questData) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-
-    a.textContent = quest.title;
-    //magic url stuff
-    a.href = `../quest/?id=${quest.id}`;
-
-    li.append(a);
-    ul.append(li);
+    if (!user.completed[quest.id]) {
+        completedAllQuests = false;
+    } 
 }
+
+if (user.hp <= 0 || completedAllQuests === true) {
+    window.location.href = '../results';
+} else {
+
+    for (const quest of questData) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        if (user.completed[quest.id]) {
+            a.textContent = `${quest.title} ✔`;
+        } else {
+            a.textContent = quest.title;
+    //magic url stuff
+            a.href = `../quest/?id=${quest.id}`;
+        }
+        li.append(a);
+        ul.append(li);
+    }}
--- quest/questUtils.js
+++ quest/questUtils.js
@@ -2,11 +2,13 @@ import { getFromLocalStorage, setToLocalStorage } from '../localStorageUtils.js'
 import { QUESTER } from '../magicStrings.js';
 import { findById } from '../utils.js';
 
-
 export function generateForm(currentQuest) {
     const inputForm = document.querySelector('section>form');
-    const button = document.createElement('button');
-    
+    const choiceResultDiv = document.querySelector('section>div');
+    const submitChoice = document.createElement('button');
+    const backToMap = document.createElement('button');
+    const user = getFromLocalStorage(QUESTER);
+
     for (const choice of currentQuest.choices) {
         const input = document.createElement('input');
         const img = document.createElement('img');
@@ -23,9 +25,9 @@ export function generateForm(currentQuest) {
         label.append(input, img);
         inputForm.append(label);
     }
-    button.textContent = "Let's go!";
+    submitChoice.textContent = "Let's go!";
     
-    inputForm.append(button);
+    inputForm.append(submitChoice);
     
     inputForm.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -35,13 +37,21 @@ export function generateForm(currentQuest) {
         // get stuff out of bundle and set it to stuff
         const selectionId = formData.get('choice');
         const choice = findById(currentQuest.choices, selectionId);
-        const user = getFromLocalStorage(QUESTER);
-
-        console.log(selectionId);
-
-        user.hp = user.hp + choice.hp;
-        user.gold = user.gold + choice.gold;
-    
+        
+        user.hp += choice.hp;
+        user.gold += choice.gold;
+        user.completed[currentQuest.id] = true;
+        
         setToLocalStorage(QUESTER, user);
+        
+        choiceResultDiv.textContent = choice.result;
+        inputForm.textContent = '';
+        backToMap.textContent = 'Back to map!';
+        
+        backToMap.addEventListener('click', () => {
+            window.location.href = '../map';
+        });
+
+        choiceResultDiv.append(backToMap);
     });
 }
