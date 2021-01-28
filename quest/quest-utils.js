95e35e61254a73e49156af07991434026b6f966b
ERICH SAUER <erich.sauer@gmail.com>
2021-01-27 20:17:16 -0800

    makes that last commit true

--- quest/quest-utils.js
+++ quest/quest-utils.js
@@ -1,7 +1,8 @@
 export function generateForm(currentQuest) {
     const inputForm = document.querySelector('section>form');
-    const choices = currentQuest.choices;
-    for (const choice of choices) {
+    const button = document.createElement('button');
+
+    for (const choice of currentQuest.choices) {
         const input = document.createElement('input');
         const img = document.createElement('img');
         const label = document.createElement('label');
@@ -13,8 +14,11 @@ export function generateForm(currentQuest) {
         img.src = `../assets/${choice.id}.png`;
 
         label.textContent = choice.description;
-
+        
         label.append(img, input);
         inputForm.append(label);
     }
+    button.textContent = "Let's go!";
+
+    inputForm.append(button);
 }
